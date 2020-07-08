78857d7f863a8c64b804abab45505ac84cf279f0
JacobZwang <zwang.jacob@gmail.com>
2020-07-08 13:12:00 -0700

    for loop using iter()

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,13 @@ fn main() {
     let stripe_flat = 0.3;
     let stripe_percent = 0.029;
 
-    let gitbid_flat = 0.3;
-    let gitbid_percent = 0.05;
+    let our_flat = 0.3;
+    let our_percent = 0.05;
 
     let price = [1.0, 2.0, 5.0, 10.0, 25.0, 50.0, 75.0, 100.0];
-    let mut index = 0;
 
-    while index < 5 {
-        let profit =
-            (gitbid_flat - stripe_flat) + ((price[index]) * (gitbid_percent - stripe_percent));
-        println!("{}$ will yield {}$", price[index], profit);
-        index += 1;
+    for price in price.iter() {
+        let profit = (our_flat - stripe_flat) + ((price) * (our_percent - stripe_percent));
+        println!("{}$ will yield {}$", price, profit);
     }
 }
